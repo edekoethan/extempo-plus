f7b67a2b4a4da1b7c71641fce0ea8122b83aca16
Edeko Eromosele <eromoseleedeko@gmail.com>
2024-09-26 14:27:57 +0100

    Updates

---
+++ precache-manifest.70ffd1d8b30e24868ea9beed8649f569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f56492ee937878de9a3f1c5070c801a",
+    "url": "/extempo-plus/index.html"
+  },
+  {
+    "revision": "470930faccfa0c3ac1e1",
+    "url": "/extempo-plus/static/css/main.35919c3a.chunk.css"
+  },
+  {
+    "revision": "7cbd725634d5b3387e32",
+    "url": "/extempo-plus/static/js/2.cd2a726b.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/extempo-plus/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "470930faccfa0c3ac1e1",
+    "url": "/extempo-plus/static/js/main.cd076b19.chunk.js"
+  },
+  {
+    "revision": "4d94c6372f6fc67df1b7",
+    "url": "/extempo-plus/static/js/runtime~main.21bf8a8f.js"
+  }
+]);
